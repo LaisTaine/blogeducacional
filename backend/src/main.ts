e98d6df3faf29e4834c6369a1f1c3fb571d65ec6
LaisTaine <lais.taine@gmail.com>
2025-09-21 22:39:07 -0300

    Correção de link Cors, no backend/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3001', 'https://blogeducacional.onrender.com/'
+      'http://localhost:3001', 'https://blogeducacional-front.onrender.com'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true, 
