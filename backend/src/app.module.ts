d396fcc384541bb9f53039e4c7f476f1fae9bc3c
LaisTaine <lais.taine@gmail.com>
2025-09-22 09:00:06 -0300

    Test: Isola o AuthModule para depuração

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,36 +1,19 @@
-import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// Em: src/app.module.ts (VERSÃO DE TESTE)
 
-import { PostsModule } from './posts/posts.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { AuthGuard } from './auth/auth.guard';
-import { RolesGuard } from './auth/roles.guard';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true, // Necessário para as variáveis de ambiente
     }),
-    PostsModule,
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
-      }),
-      inject: [ConfigService],
-    }),
-
-    UserModule,
-    AuthModule,
+    UserModule, // O AuthModule depende do UserModule, então precisamos dele
+    AuthModule, // <-- O MÓDULO QUE ESTAMOS TESTANDO
   ],
-  controllers: [AppController],
-  providers: [
-    AppService
-  ],
-}) 
+  controllers: [],
+  providers: [],
+})
 export class AppModule {}
